d5c8ad91a34b655d4ad59497ec87f02cd7ea2029
Jonesy1697 <Joneschris1697@gmail.com>
2018-02-15 11:53:28 +0000

    Removal of unsued  imports

--- AdminSystem/src/databaseConnect.java
+++ AdminSystem/src/databaseConnect.java
@@ -1,16 +1,11 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
